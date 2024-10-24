ba03a45f33e6d706db39a66467c3369b3b176afa
aishatahir3023 <160772524+aishatahir3023@users.noreply.github.com>
2024-10-25 02:30:23 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,20 +1,12 @@
 import Link from "next/link"
-
-export default function Home() {
-  return (
-   <div>
-     <h1 style={{fontSize: "50px"}}>Welcome to My first Assignment</h1>
-  
-     <Link href="/students" style={{fontSize:"50px"}}>Students</Link> <br>
-     </br>
-     <Link href="/employees" style={{fontSize:"50px"}}>Employees</Link>
-     <footer>
-    
-     </footer>
-
-    
- 
-    </div>
-  );
+const Students =  () => {
+    return (
+        <div>
+            <h1>Students List Page</h1>
+            <Link href="">
+            </Link>
+        </div>
+    )
 }
 
+export default Students
